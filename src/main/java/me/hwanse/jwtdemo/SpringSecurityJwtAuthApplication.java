7568b0c9bcb46364c4c48381a8d7babb0b6a35c9
hwanse <khsb2012@gmail.com>
2021-05-26 16:22:03 +0900

    Initial commit

---
+++ src/main/java/me/hwanse/jwtdemo/SpringSecurityJwtAuthApplication.java
@@ -0,0 +1,13 @@
+package me.hwanse.jwtdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwtAuthApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringSecurityJwtAuthApplication.class, args);
+  }
+
+}
---
+++ src/test/java/me/hwanse/jwtdemo/SpringSecurityJwtAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hwanse.jwtdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtAuthApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
