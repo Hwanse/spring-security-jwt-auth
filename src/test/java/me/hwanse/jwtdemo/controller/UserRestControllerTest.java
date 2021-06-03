e3591eff24d7b965264562a31982dc3d2da70bd9
hwanse <khsb2012@gmail.com>
2021-06-03 16:56:29 +0900

    test: 로그인, 회원가입 API 테스트 및 Jwt 발급 테스트 코드 작성

---
+++ src/test/java/me/hwanse/jwtdemo/controller/UserRestControllerTest.java
@@ -0,0 +1,53 @@
+package me.hwanse.jwtdemo.controller;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import me.hwanse.jwtdemo.controller.dto.JoinRequest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class UserRestControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Autowired
+  private ObjectMapper objectMapper;
+
+  @Test
+  @DisplayName("API 회원 가입 테스트")
+  public void join() throws Exception {
+    // given
+    JoinRequest joinRequest = new JoinRequest("test@gmail.com", "test",
+      "test", "test");
+
+    // when
+    mockMvc.perform(post("/api/join")
+          .contentType(MediaType.APPLICATION_JSON_VALUE)
+          .content(objectMapper.writeValueAsString(joinRequest)))
+      .andExpect(status().isOk())
+      .andExpect(jsonPath("$.success", is(true)))
+      .andExpect(jsonPath("$.response.email", is(joinRequest.getEmail())))
+      .andExpect(jsonPath("$.response.name", is(joinRequest.getUsername())))
+      .andExpect(jsonPath("$.response.nickname", is(joinRequest.getNickname())))
+      .andExpect(jsonPath("$.response.authorities.[0].name", is("ROLE_USER")))
+      .andExpect(jsonPath("$.response.createAt").exists())
+      .andExpect(jsonPath("$.response.modifiedAt").exists())
+      .andExpect(jsonPath("$.response.deletedAt").isEmpty())
+      .andExpect(jsonPath("$.response.beDeleted", is(false)))
+      .andExpect(jsonPath("$.error").doesNotExist());
+
+  }
+
+}
