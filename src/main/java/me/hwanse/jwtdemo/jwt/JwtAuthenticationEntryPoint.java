5505482868d105df4af69fe9cfec4a592468dbde
hwanse <khsb2012@gmail.com>
2021-06-04 01:13:07 +0900

    feat: 예외 처리 핸들러 추가

--- src/main/java/me/hwanse/jwtdemo/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/me/hwanse/jwtdemo/jwt/JwtAuthenticationEntryPoint.java
@@ -1,9 +1,15 @@
 package me.hwanse.jwtdemo.jwt;
 
+import static me.hwanse.jwtdemo.controller.ApiResult.ERROR;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import me.hwanse.jwtdemo.controller.ApiResult;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -11,10 +17,23 @@ import org.springframework.stereotype.Component;
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
+  private final ApiResult<?> errorResponse = ERROR(HttpStatus.UNAUTHORIZED, "Authentication error (cause: unauthorized)");
+
+  private final ObjectMapper objectMapper;
+
+  public JwtAuthenticationEntryPoint(ObjectMapper objectMapper) {
+    this.objectMapper = objectMapper;
+  }
+
   @Override
   public void commence(HttpServletRequest httpServletRequest,
     HttpServletResponse httpServletResponse, AuthenticationException e)
     throws IOException, ServletException {
-    httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+
+    httpServletResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+    httpServletResponse.setContentType(MediaType.APPLICATION_JSON_VALUE);
+    httpServletResponse.getWriter().write(objectMapper.writeValueAsString(errorResponse));
+    httpServletResponse.getWriter().flush();
+    httpServletResponse.getWriter().close();
   }
 }
