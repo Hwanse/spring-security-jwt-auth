6ea8f476ea0ca5502491340c614e6d47375ce528
hwanse <khsb2012@gmail.com>
2021-06-03 20:44:18 +0900

    feat: 로그인, 회원가입 요청 URL은 JWT Filter 에서 예외시키도록 수정 및 예외 케이스에 대한 테스트 코드 추가

--- src/main/java/me/hwanse/jwtdemo/jwt/JwtAuthenticationFilter.java
+++ src/main/java/me/hwanse/jwtdemo/jwt/JwtAuthenticationFilter.java
@@ -4,23 +4,22 @@ import io.jsonwebtoken.Claims;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Optional;
 import java.util.stream.Collectors;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.util.StringUtils;
-import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 @Slf4j
-public class JwtAuthenticationFilter extends GenericFilterBean {
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
   public static final String AUTHORIZATION_HEADER = "Authorization";
   public static final String BEARER = "Bearer";
@@ -32,7 +31,9 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
   }
 
   @Override
-  public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+    FilterChain filterChain) throws ServletException, IOException {
+
     HttpServletRequest httpServletRequest = (HttpServletRequest) request;
     String requestURI = httpServletRequest.getRequestURI();
 
@@ -46,20 +47,38 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
         String roles = claims.get("roles", String.class);
         Collection<? extends GrantedAuthority> authorities =
           Arrays.stream(roles.split(","))
-                .map(SimpleGrantedAuthority::new)
-                .collect(Collectors.toSet());
+            .map(SimpleGrantedAuthority::new)
+            .collect(Collectors.toSet());
 
         if (validateClaims(username, email, roles)) {
           JwtAuthenticationToken jwtAuthenticationToken = new JwtAuthenticationToken(
             new User(email, null, authorities), null, authorities);
           jwtAuthenticationToken.setDetails(token);
           SecurityContextHolder.getContext().setAuthentication(jwtAuthenticationToken);
-          log.debug("인증 정보를 저장했습니다. '{}' / URI : {}", jwtAuthenticationToken.getPrincipal(), requestURI);
+          log.debug("인증 정보를 저장했습니다. '{}' / URI : {}", jwtAuthenticationToken.getPrincipal(),
+            requestURI);
         }
       }
     }
 
     filterChain.doFilter(request, response);
+
+  }
+
+  /**
+   * 요청 URL이 '/api/login', '/api/join'일 경우 필터를 거치지 않는다.
+   * true 리턴 시 다음 필터로 skip, false 리턴 시 이 Filter 내부에 정의한 doFilterInternal 를 거친다.
+   */
+  @Override
+  protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+    AntPathMatcher pathMatcher = new AntPathMatcher();
+    String requestURI = request.getRequestURI();
+
+    if (pathMatcher.match("/api/login", requestURI) || pathMatcher.match("/api/join", requestURI)) {
+      return true;
+    }
+
+    return false;
   }
 
   private boolean validateClaims(String username, String email, String roles) {
